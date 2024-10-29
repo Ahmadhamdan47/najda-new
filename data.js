d94cf5b4cc6cdbaa7be8ae5b43827f19e588fa75
Ahmadhamdan47 <ahmadhamdan47@gmail.com>
2024-10-29 15:54:24 +0200

    data entry and database

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const xlsx = require('xlsx');
 
 // Load the Excel file
-const workbook = xlsx.readFile('data.xlsx');
+const workbook = xlsx.readFile('najdata.xlsx');
 
 // Connect to MySQL
 const connection = mysql.createConnection({
@@ -18,31 +18,8 @@ connection.connect(err => {
 });
 
 // Get data from each sheet
-const housesSheet = xlsx.utils.sheet_to_json(workbook.Sheets['Sheet1']);
 const familiesSheet = xlsx.utils.sheet_to_json(workbook.Sheets['Sheet2']);
-const needsSheet = xlsx.utils.sheet_to_json(workbook.Sheets['Sheet3']);
 
-// Insert data into Houses table
-housesSheet.forEach(row => {
-  const sql = `INSERT INTO houses (house_label, total_people, under_two_years, under_thirteen_years, over_thirteen_years, current_residence, displacement_location, furnished_home, gas)
-               VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-  const values = [
-    row['الاسم'],  // Adjust column names as needed
-    row['عدد الاشخاص'],
-    row['الفئة العمرية (تحت السنتين)'],
-    row['تحت 13 سنة'],
-    row['فوق ال13 سنة'],
-    row['مكان الاقامة الحالية'],
-    row['مكان النزوح'],
-    row['منزل مفروش'] === 'نعم',  // Convert to boolean
-    row['غاز'] === 'بحاجة'        // Convert to boolean
-  ];
-
-  connection.query(sql, values, (err, result) => {
-    if (err) throw err;
-    console.log('House data inserted');
-  });
-});
 
 // Insert data into Families table
 familiesSheet.forEach(row => {
@@ -70,23 +47,9 @@ familiesSheet.forEach(row => {
   });
 });
 
-// Insert data into Needs table
-needsSheet.forEach(row => {
-  const sql = `INSERT INTO needs (food_aid, medicine, furniture, clothes, cleaning_supplies)
-               VALUES (?, ?, ?, ?, ?)`;
-  const values = [
-    row['حصة غذائية'] === 'نعم',  // Convert to boolean
-    row['ادوية'] === 'نعم',  // Convert to boolean
-    row['فرش'] === 'نعم',  // Convert to boolean
-    row['ملابس'] === 'نعم',  // Convert to boolean
-    row['مواد تنظيف'] === 'نعم'  // Convert to boolean
-  ];
 
-  connection.query(sql, values, (err, result) => {
-    if (err) throw err;
-    console.log('Needs data inserted');
-  });
-});
+
+
 
 // Close the MySQL connection
 connection.end(err => {
